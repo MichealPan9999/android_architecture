a0d84150fab5befb0885b0e3469bf24a7246d645
MichealPan9999 <panziqiang99@gmail.com>
2018-09-26 18:38:37 +0800

    设计mvc

---
+++ mvc/src/main/java/com/example/mvc/model/OnLoadWeatherCallback.java
@@ -0,0 +1,8 @@
+package com.example.mvc.model;
+
+import com.android.libcore.net.NetError;
+
+public interface OnLoadWeatherCallback {
+    void onLoadSuccess(WeatherInfo info);
+    void onError(NetError error);
+}
